53eadf525fade56e8a457a450c6b189f580103f9
Evgeniy Parmon <laneqan1@gmail.com>
2017-05-14 22:16:38 +0300

    startup page

--- CourseProject/App_Start/RouteConfig.cs
+++ CourseProject/App_Start/RouteConfig.cs
@@ -14,10 +14,12 @@ namespace CourseProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            "Default", 
+            "{controller}/{action}/{id}", // URL with parameters
+            new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+        );
+
         }
+
     }
 }
